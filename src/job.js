5990613b4515037ba23e653621327c1cd98d9cfd
Jonathan Dupré <hello@jonathandupre.com>
2020-06-30 22:06:35 -0400

    cool

--- src/job.js
+++ src/job.js
@@ -1,1 +1,9 @@
-console.log(process.env.GOOGLE_API_KEY||'xxxxxx'.slice(0,5) + '****')
+console.log('starting job...')
+
+console.log('fetching spreadsheet...')
+
+console.log('updating playlist...')
+
+console.log('posting on facebook...')
+
+console.log('all done!')
